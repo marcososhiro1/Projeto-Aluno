2f192377ce1e217a7fa8a8e575801416b9e4ccac
Marcos Oshiro <marcososhiro66@gmail.com>
2024-03-15 01:36:22 -0300

    enviando primeiro arquivo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#2C3054',
+        secondary:'#193CB8',
+        third: '#4151DD'
+      }
+    },
+  },
+  plugins: [],
+}
+
