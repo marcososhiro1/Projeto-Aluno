c4b3037b74eac30549c097b27a34aa1352d26e15
Marcos Oshiro <marcososhiro66@gmail.com>
2024-04-11 06:12:39 -0300

    enviando o botão de configuração

--- src/script.js
+++ src/script.js
@@ -11,3 +11,17 @@ btn.addEventListener('click', function(){
         btnesconder.style.display = 'block';
     }
 })
+
+//dropdown do botão de configuração
+
+const btndropdownConfig = document.getElementById('btndropdownConfig')
+let uldropConfig = document.getElementById('uldropConfig')
+
+btndropdownConfig.addEventListener('click', function(){
+    if(uldropConfig.style.display == 'block'){
+        uldropConfig.style.display = 'none';
+    }
+    else{
+        uldropConfig.style.display = 'block';
+    }
+})
