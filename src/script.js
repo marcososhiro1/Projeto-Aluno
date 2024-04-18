23c1ec24957be60b259b5fb95597a8dc957334a8
Marcos Oshiro <marcososhiro66@gmail.com>
2024-04-18 13:32:49 -0300

    Telas

--- src/script.js
+++ src/script.js
@@ -1,16 +1,3 @@
-//esconder e aparecer botao sair
-
-const btn = document.getElementById('show-or-hide')
-let btnesconder = document.getElementById('botao_logout')
-
-btn.addEventListener('click', function(){
-    if(btnesconder.style.display == 'block'){
-        btnesconder.style.display = 'none';
-    }
-    else{
-        btnesconder.style.display = 'block';
-    }
-})
 
 //dropdown do botão de configuração
 
