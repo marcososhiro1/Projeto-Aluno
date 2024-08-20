0f174b6825675ab52a83a1b5ba91af9be3f3c192
Marcos Oshiro <marcososhiro66@gmail.com>
2024-08-20 12:20:04 -0300

    enviando projeto provisorio de cadastro de alunos e usuarios

--- src/script.js
+++ src/script.js
@@ -1,4 +1,23 @@
 
+
+//CRUD -Gestao
+
+// enviar o array para o local storage
+//pegar os dados do local e transportar para o html
+// estilizar os dados 
+
+let pessoa = [
+    {ID:"0000", nome: "Amanda"},
+    {ID:"0001", nome: "Marcos"},
+    {ID:"0002", nome: "Daniela"},
+    {ID:"0003", nome: "Jurandi"},
+    {ID:"0004", nome: "Maria"}
+]
+
+
+
+
+
 //dropdown do botão de configuração
 
 const btndropdownConfig = document.getElementById('btndropdownConfig')
